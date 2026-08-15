fcdef01781b4cdca68c9703cb560dfa2b16ddf3f
Carina Delay <carina.harumi.2101@gmail.com>
2026-08-15 22:47:59 +0900

    CH4 block_markdown partial

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -71,7 +71,7 @@ def _text_to_children(text: str) -> list[HTMLNode]:
 
 # Type helper function structure:
 # get rid of the markdown markers
-# call _text_to_children on the cleaned up text to get the inline children
+# call _text_to_children on the cleaned up text to get the inline children (except codeblock)
 # attach the little LeafNodes to a ParentNode with the corresponding HTML tag
 # return the parent node
 
@@ -91,6 +91,54 @@ def _heading_to_html_node(heading: str) -> ParentNode:
     heading_leaf_nodes = _text_to_children(clean_heading)
     heading_branch_node = ParentNode(tag, heading_leaf_nodes, None)
     return heading_branch_node
+
+def _quote_to_html_node(quote: str) -> ParentNode:
+    # Kinda like headings
+    quote_lines = quote.split("\n")
+    clean_quote_lines = []
+    for line in quote_lines:
+        clean_quote_lines.append(line.removeprefix(">").removeprefix(" "))
+    # Because quotes are blocks of paragraph, formatted line by line only because markdown
+    quote_block = " ".join(clean_quote_lines)
+    quote_leaf_node = _text_to_children(quote_block)
+    quote_branch_node = ParentNode("blockquote", quote_leaf_node, None)
+    return quote_branch_node
+
+def _unordered_list_to_html_node(unordered_list: str) -> ParentNode:
+    list_items = unordered_list.split("\n")
+    clean_list_items = []
+    for item in list_items:
+        clean_list_items.append(item.removeprefix("- "))
+    item_branches_li = []
+    for cleaned_item in clean_list_items:
+        item_leaf_node = _text_to_children(cleaned_item)
+        item_branch_li = ParentNode("li", item_leaf_node, None)
+        item_branches_li.append(item_branch_li)
+    unordered_list_node = ParentNode("ul", item_branches_li, None)
+    return unordered_list_node
+
+def _ordered_list_to_html_node(ordered_list: str) -> ParentNode:
+    # Like unordered lists, but need a different prefix logic
+    list_items = ordered_list.split("\n")
+    clean_list_items = []
+    # manual counter works here too, but enumerate is an awesome tool
+    for i, item in enumerate(list_items, start=1):
+        clean_list_items.append(item.removeprefix(f"{i}. "))
+    item_branches_li = []
+    for cleaned_item in clean_list_items:
+        item_leaf_node = _text_to_children(cleaned_item)
+        item_branch_li = ParentNode("li", item_leaf_node, None)
+        item_branches_li.append(item_branch_li)
+    ordered_list_node = ParentNode("ol", item_branches_li, None)
+    return ordered_list_node
+
+def _code_to_html_node(code: str) -> ParentNode:
+    clean_code = code.removeprefix("```\n").removesuffix("```")
+    code_text_node = TextNode(clean_code, "code", None)
+    code_html_node = text_node_to_html_node(code_text_node)
+##########################################################################
+
+
 
 
 def markdown_to_html_node(markdown):
