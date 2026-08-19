17b05325eef4e85b4e1d6e3ff63048bd3d5dc02d
Carina Delay <carina.harumi.2101@gmail.com>
2026-08-19 16:05:09 +0900

    CH5 L5: Deploy to the internet

--- src/gencontent.py
+++ src/gencontent.py
@@ -9,7 +9,7 @@ def extract_title(markdown: str) -> str:
     return markdown.split("# ", maxsplit=1)[1]
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 # read() needs a file object, so we need to turn the path string into file objects
     with open(from_path, mode='r') as f:
@@ -22,6 +22,8 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     # replace the title, then replace the content, reassigning variables because replace() doesn't modify things in place
     template_text = template_text.replace("{{ Title }}", page_title)
     template_text = template_text.replace("{{ Content }}", page_content)
+    template_text = template_text.replace('href="/', 'href="/' + basepath)
+    template_text = template_text.replace('src="/', 'src="/' + basepath)
     # Write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist.
     # make dirs, open, write
     dest_path_dirname = os.path.dirname(dest_path)
@@ -31,17 +33,17 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
         f.write(template_text)
 
 
-def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str) -> None:
     content_files = os.listdir(dir_path_content)
     for file in content_files:
         content_file_path = os.path.join(dir_path_content, file)
         dest_file_path = os.path.join(dest_dir_path, file)
         if not os.path.isfile(content_file_path):
-            generate_pages_recursive(content_file_path, template_path, dest_file_path)
+            generate_pages_recursive(content_file_path, template_path, dest_file_path, basepath)
         else:
             if file.endswith(".md"):
                 # string methods work but let's learn how to use more robust tools
                 p = Path(dest_file_path)
                 html_path = p.with_suffix(".html")
                 str_html_path = str(html_path)
-                generate_page(content_file_path, template_path, str_html_path)
+                generate_page(content_file_path, template_path, str_html_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
+import sys
 from textnode import TextNode
 from textnode import TextType
 from copystatic import copystatic
 from gencontent import generate_page, generate_pages_recursive
 
 source_path = "./static"
-destination_path = "./public"
+destination_path = "./docs"
 from_path = "./content"
 template_path = "./template.html"
-dest_path = "./public"
+default_basepath = "/"
 
 # If the program was a factory, main.py is the operation instructions for a machine that sits in a separate room (main.sh)
 # so the paths we assign to variables start assume the POV of the machine, not of the file cabinet where the operations are stored.
 def main() -> None:
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     copystatic(source_path, destination_path)
-    generate_pages_recursive(from_path, template_path, dest_path)
+    generate_pages_recursive(from_path, template_path, destination_path, basepath)
 
 
 main()
